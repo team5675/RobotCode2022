d1e3ad489f1a35c4dc0cd9beb457d77f7df62323
Sargeantfish <sargeantfishcoc@gmail.com>
2022-01-18 15:40:40 -0500

    2022 Stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -53,7 +53,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+
+    
+  }
 
   /** This function is called periodically during operator control. */
   @Override
