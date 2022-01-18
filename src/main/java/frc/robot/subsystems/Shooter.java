f9004b30d1e43eeab70c134078d8bd385d594d52
ReturnToBeans <96270156+ReturnToBeans@users.noreply.github.com>
2022-01-18 18:40:13 -0500

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,8 +1,24 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Spark;
+import frc.libs.motors.SparkMaxMotor;
+import frc.robot.Constants;
+import frc.robot.DriverController;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class Shooter{
 
     static Shooter instance;
+
+    enum ShooterState{
+
+        StartUp,
+        Shooting,
+        Idle
+    }
 
     public Shooter getInstance(){
 
