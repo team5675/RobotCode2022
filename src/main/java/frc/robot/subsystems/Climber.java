82eb147873dc33552fc673fc1a32a65dea9769af
Ava Ulberg <aulberg984@gmail.com>
2022-02-05 10:39:57 -0500

    Shuffleboard Climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,38 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
 public class Climber{
+
+    public static void updateShuffleboard(){
+
+        Shuffleboard.getTab("Climber")
+        .add("Limit Switch1", 1)
+        .withWidget(BuiltInWidgets.kBooleanBox)
+        .getEntry();
+
+        Shuffleboard.getTab("Climber")
+        .add("Limit Switch2", 1)
+        .withWidget(BuiltInWidgets.kBooleanBox)
+        .getEntry();
+
+        Shuffleboard.getTab("Climber")
+        .add("Cylinder1", 1)
+        .withWidget(BuiltInWidgets.kRelay)
+        .getEntry();
+
+        Shuffleboard.getTab("Climber")
+        .add("Cylinder2", 1)
+        .withWidget(BuiltInWidgets.kRelay)
+        .getEntry();
+
+        Shuffleboard.getTab("Climber")
+        .add("Cylinder3", 1)
+        .withWidget(BuiltInWidgets.kRelay)
+        .getEntry();
+    }
 
     static Climber instance;
 
