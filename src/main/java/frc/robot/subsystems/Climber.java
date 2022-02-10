fc4b9cb5e2e420f9879689795e90bfb72ef5e178
Ava Ulberg <aulberg984@gmail.com>
2022-02-09 19:54:54 -0500

    Climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -34,6 +34,43 @@ public class Climber{
         .getEntry();
     }
 
+
+    DigitalInput limitSwitch1 = new DigitalInput(0);
+    DigitalInput limitSwitch2 = new DigitalInput(1);
+    .getChannel();
+    
+    
+    
+    
+    //Code until robot is assembled
+   /* public void teleopPeriodic() {
+        setMotorSpeed(joystick.getRawAxis(2));
+    }
+    
+    public void setMotorSpeed(double speed) {
+        if (speed > 0) {
+            if (toplimitSwitch.get()) {
+                // We are going up and top limit is tripped so stop
+                motor.set(0);
+            } else {
+                // We are going up but top limit is not tripped so go at commanded speed
+                motor.set(speed);
+            }
+        } else {
+            if (bottomlimitSwitch.get()) {
+                // We are going down and bottom limit is tripped so stop
+                motor.set(0);
+            } else {
+                // We are going down but bottom limit is not tripped so go at commanded speed
+                motor.set(speed);
+            }
+        }
+    }
+*/
+
+
+
+
     static Climber instance;
 
     public Climber getInstance(){
